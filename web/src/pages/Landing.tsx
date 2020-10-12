6d540ddd3972cc397992631188ab208f6ef7eaa0
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-12 15:05:58 -0300

    #web - Created app routes

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import '../styles/pages/landing.css';
+
+import logoImg from '../images/logo.svg';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt=""/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Arapiraca</strong>
+          <span>Alagoas</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default Landing;
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function OrphanagesMap() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default OrphanagesMap;
