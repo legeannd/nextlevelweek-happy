d382a011c196bbebbf40bf1c917061617d183f49
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-12 15:35:12 -0300

    #web - Using leaflet to show map on orphanages map page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -23,7 +26,14 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <Map 
+        center={[-9.6987457,-36.5769928]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={52} color="#fff" />
