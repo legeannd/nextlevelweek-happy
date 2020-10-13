8d3966ba27260c04a0909976bcd9e683f0aa6c71
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-13 11:54:22 -0300

    #server - Using Yup to validate fields + created  error handler + added cors

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
 
@@ -42,9 +43,9 @@ export default {
     const requestImages = request.files as Express.Multer.File[];
     const images = requestImages.map(image => {
       return { path: image.filename }
-    })
-  
-    const orphanage = orphanagesRepository.create({
+    });
+
+    const data = {
       name, 
       latitude,
       longitude,
@@ -53,7 +54,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+  
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
