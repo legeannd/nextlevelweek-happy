ad37d7c4bd3712af7fb78830ac388a7578604d5c
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-13 10:25:18 -0300

    #server - Listing all orphanages

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,6 +3,14 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name, 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
