7e67b18a55e421597d1d56ad9fa1d249dfb41cf6
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-13 10:21:58 -0300

    #server - Abstrated creation to controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async create(request: Request, response: Response) {
+    const {
+      name, 
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
+  
+    const orphanagesRepository = getRepository(Orphanage);
+  
+    const orphanage = orphanagesRepository.create({
+      name, 
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    });
+  
+    await orphanagesRepository.save(orphanage);
+  
+    return response.status(201).json(orphanage);
+  }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,13 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
 
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/orphanages', async (request, response) => {
-  const {
-    name, 
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name, 
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-})
+app.use(routes);
 
 app.listen(3333);
