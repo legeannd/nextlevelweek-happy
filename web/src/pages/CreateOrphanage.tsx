59665cf6ae904ecf0e5b446d9f9908d248420bec
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-14 19:55:46 -0300

    #web - Changing map marker on click in create orphanage page map component

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import { FiPlus } from "react-icons/fi";
 
@@ -8,6 +9,16 @@ import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 
 export default function CreateOrphanage() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0})
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng;
+
+    setPosition({
+      latitude: lat, 
+      longitude: lng,
+    });
+  }
 
   return (
     <div id="page-create-orphanage">
@@ -22,12 +33,23 @@ export default function CreateOrphanage() {
               center={[-27.2092052,-49.6401092]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onClick={handleMapClick}
             >
               <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              {position.latitude !== 0 && (
+                <Marker 
+                  interactive={false} 
+                  icon={mapIcon} 
+                  position={[
+                    position.latitude, 
+                    position.longitude
+                  ]}
+                />
+              )}
+
             </Map>
 
             <div className="input-block">
