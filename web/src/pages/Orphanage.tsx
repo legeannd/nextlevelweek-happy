40bad7967ef40d2877f9d0ad8508c7a0e0938448
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-14 19:46:48 -0300

    #web - Changing between images in orphanage details page

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -30,6 +30,7 @@ interface OrphanageParams {
 export default function Orphanage() {
   const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
   useEffect(() => {
     api.get(`orphanages/${params.id}`).then(response => {
@@ -47,12 +48,19 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[0].url} alt={orphanage.name} />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            {orphanage.images.map(image => {
+            {orphanage.images.map((image, index) => {
               return (
-                <button key={image.id} className="active" type="button">
+                <button 
+                  key={image.id} 
+                  className={activeImageIndex === index ? 'active' : ''} 
+                  type="button"
+                  onClick={() => {
+                    setActiveImageIndex(index);
+                  }}
+                >
                   <img src={image.url} alt={orphanage.name} />
                 </button>
               );
